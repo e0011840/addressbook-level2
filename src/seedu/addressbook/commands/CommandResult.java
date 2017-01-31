0761d12a507c653e6a72e54cf0534096215c2154
Difeng Tang <Difeng Tang>
2017-01-31 17:43:45 +0800

    [LO-Encapsulation]

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -10,8 +10,11 @@ import java.util.Optional;
  */
 public class CommandResult {
 
-    /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    /**
+     * The feedback message to be shown to the user. Contains a description of
+     * the execution result
+     */
+    private final String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
@@ -31,6 +34,10 @@ public class CommandResult {
      */
     public Optional<List<? extends ReadOnlyPerson>> getRelevantPersons() {
         return Optional.ofNullable(relevantPersons);
+    }
+
+    public String getFeedbackToUser() {
+        return this.feedbackToUser;
     }
 
 }
