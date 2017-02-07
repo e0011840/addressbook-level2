131db575417d60603351abbc905293725273a4e6
Difeng Tang <Difeng Tang>
2017-02-07 23:23:41 +0800

    [LO-Interface Segregation P] Printable.java

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Interface for printing Name, Phone, Email, Address
+ */
+
+public interface Printable {
+    
+    public String getPrintableString();
+    
+}
